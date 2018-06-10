8ef346ca479c8582a9d6cb8fcf627f16b9f794a4
oleg201119 <oleg201119@yandex.com>
2018-06-10 21:59:13 +0700

    Add image.

--- src/resource/Images.js
+++ src/resource/Images.js
@@ -1,17 +1,17 @@
 const images = {
   /* logo */
-  ic_logo_big: "/img/image-logo-big@2x.png",
-  ic_logo_small: "/img/image-logo-small@2x.png",
+  ic_logo_big: "/img/image-logo-big.png",
+  ic_logo_small: "/img/image-logo-small.png",
 
   /* category */
-  ic_bike_angle: "/img/image-bike-red-angle@2x.png",
-  ic_bike_horiz: "/img/image-bike-red-horiz@2x.png",
+  ic_bike_angle: "/img/bike-red-angle.png",
+  ic_bike_horiz: "/img/bike-red-horiz.png",
   ic_running_dark: "/img/image-running-dark@2x.png",
   ic_running_red: "/img/image-running-red@2x.png",
 
   /* laps */
-  ic_cup: "/img/image-cup@2x.png",
-  ic_timer: "/img/image-timer@2x.png",
+  ic_cup: "/img/image-cup.png",
+  ic_timer: "/img/image-timer.png",
 
   /* background */
   ic_home_background: "/img/image-home-bg@2x.png"
