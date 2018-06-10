82a113b3b2b6e4331dc56b28369e8c265edd3cd7
oleg201119 <oleg201119@yandex.com>
2018-06-10 22:19:10 +0700

    change image source.

--- src/resource/Images.js
+++ src/resource/Images.js
@@ -6,15 +6,15 @@ const images = {
   /* category */
   ic_bike_angle: "/img/bike-red-angle.png",
   ic_bike_horiz: "/img/bike-red-horiz.png",
-  ic_running_dark: "/img/image-running-dark@2x.png",
-  ic_running_red: "/img/image-running-red@2x.png",
+  ic_running_dark: "/img/image-running-dark.png",
+  ic_running_red: "/img/image-running-red.png",
 
   /* laps */
   ic_cup: "/img/image-cup.png",
   ic_timer: "/img/image-timer.png",
 
   /* background */
-  ic_home_background: "/img/image-home-bg@2x.png"
+  ic_home_background: "/img/login-bg.png"
 };
 
 export default images;
